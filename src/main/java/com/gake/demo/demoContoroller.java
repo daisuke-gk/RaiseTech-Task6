20f875c5ca7bc0530207f8e6bcca161708f34966
daisuke-gk <daidai481.gake@gmail.com>
2023-10-13 18:29:11 +0900

    時間を返す機能を追加

--- src/main/java/com/gake/demo/demoContoroller.java
+++ src/main/java/com/gake/demo/demoContoroller.java
@@ -3,11 +3,33 @@ package com.gake.demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @RestController
 public class demoContoroller {
 
     @GetMapping("/hello")
-    public String hello(){
-        return "Hello World";
+    public String hello() {
+        return "hello world";
     }
+
+    @GetMapping("/time")
+    public LocalDateTime time() {
+
+        return LocalDateTime.now();
+    }
+
+    @GetMapping("/time2")
+    public String time2() {
+        LocalDateTime now = LocalDateTime.now();
+
+        DateTimeFormatter f = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+
+        return ("今の時間は：" + now.format(f) + " です。");
+    }
+
+
 }
+
+
